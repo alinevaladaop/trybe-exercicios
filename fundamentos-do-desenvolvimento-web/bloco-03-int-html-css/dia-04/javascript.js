c2facc7e183cd8ce7733dd2ce376ec6cbe1790e3
Aline Valadao <valadao.aline@gmail.com>
2022-05-13 21:08:40 +0200

    Primeiro Exercicioclear

---
+++ fundamentos-do-desenvolvimento-web/bloco-03-int-html-css/dia-04/javascript.js
@@ -0,0 +1,15 @@
+const myName = "Aline";
+const  birthCity = "Brasília";
+let birthYear = 1989;
+
+console.log (myName);
+console.log (birthCity);
+console.log (birthYear);
+
+birthYear = 2030;
+console.log (birthYear);
+
+birthCity = "São Paulo"; //TypeError: Assignment to constant variable.
+console.log (birthCity); 
+
+
