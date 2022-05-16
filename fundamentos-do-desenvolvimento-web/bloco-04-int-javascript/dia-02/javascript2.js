fcc165070d21702c2d3bfa465033b3db6673ef76
Aline Valadao <valadao.aline@gmail.com>
2022-05-16 18:18:39 +0200

    Primeiro e segundo exercicio

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-int-javascript/dia-02/javascript2.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultadoSoma=0; 
+
+
+for (index = 0; index < numbers.length; index +=1){
+    resultadoSoma = resultadoSoma + (numbers[index]);
+}
+
+console.log (resultadoSoma);
