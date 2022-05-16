a713d6d1dffeceac9d71ebd5ff783b7aa16f39c5
Aline Valadao <valadao.aline@gmail.com>
2022-05-16 15:28:47 +0200

    listaExercicio3

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-int-javascript/dia-01/listaExercicio3.js
@@ -0,0 +1,11 @@
+const a = 25
+const b = 30
+const c = 59
+
+if (a > b && a > c) {
+    console.log ("a é o maior número")
+} else if(b > a && b > c){
+    console.log ("b é o maior número")
+} else {
+    console.log ("c é o maior número")
+}
