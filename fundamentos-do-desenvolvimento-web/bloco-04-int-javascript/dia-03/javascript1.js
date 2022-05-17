0174d07018333f3c3cfc4c36a3166c2d4ce58a33
Aline Valadao <valadao.aline@gmail.com>
2022-05-17 13:39:58 +0200

    primeiro exercicio

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-int-javascript/dia-03/javascript1.js
@@ -0,0 +1,10 @@
+//Crie um algoritmo que retorne o fatorial de 10.
+
+let numbers = 10;
+let fatorial = 1;
+
+for (let index = 1; index <= 10; index += 1){
+    fatorial = fatorial * index
+}
+
+console.log (fatorial);
