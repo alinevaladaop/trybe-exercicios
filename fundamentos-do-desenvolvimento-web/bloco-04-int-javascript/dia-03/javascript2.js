c8312c7b40a84d0caecd20e1fdbf73c36bb9f469
Aline Valadao <valadao.aline@gmail.com>
2022-05-17 14:07:18 +0200

    segundo exercicio

---
+++ fundamentos-do-desenvolvimento-web/bloco-04-int-javascript/dia-03/javascript2.js
@@ -0,0 +1,9 @@
+//inverta a palavra
+let word = 'tryber';
+let invertida = ''
+
+for (let index = 0; index < word.length; index += 1){
+    invertida = invertida + word [word.length - 1 - index]
+}
+
+console.log (invertida);
